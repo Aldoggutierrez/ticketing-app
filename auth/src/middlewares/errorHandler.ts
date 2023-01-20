4c37c5eeef069bdd096377f8d3a1a9ebbb4744f0
Aldoggutierrez <55823142+Aldoggutierrez@users.noreply.github.com>
2023-01-20 06:34:27 +0000

    added error classes

--- auth/src/middlewares/errorHandler.ts
+++ auth/src/middlewares/errorHandler.ts
@@ -1,7 +1,16 @@
 import { Request,Response, NextFunction } from "express";
+import { DatabaseConnectionError } from "../errors/databaseConnectionError";
+import { RequestValidationError } from "../errors/requestValidationError";
 
 export const errorHnadler = (err: Error,req: Request,res: Response,next: NextFunction) => {
-    //console.log('Somting went wrong', err);
+    if (err instanceof RequestValidationError) {
+        console.log('handling this as a request validation error');
+    }
+    
+    if (err instanceof DatabaseConnectionError) {
+        console.log('handling this as a database connection error');
+    }
+
     res.status(400).send({
         message: err.message
     })
